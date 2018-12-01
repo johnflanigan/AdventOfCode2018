311e8e59a84d06f9963679ea07c7306952733783
John Flanigan <johnrflanigan@gmail.com>
2018-12-01 02:54:06 -0500

    Completed day 1

---
+++ src/main/java/net/johnflanigan/Main.java
@@ -0,0 +1,12 @@
+package net.johnflanigan;
+
+import net.johnflanigan.day1.day1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(day1.day1());
+
+    }
+}
---
+++ src/main/java/net/johnflanigan/day1/day1.java
@@ -0,0 +1,48 @@
+package net.johnflanigan.day1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+public class day1 {
+
+    public static String day1() {
+
+        String file ="src/main/resources/day1_input.txt";
+        List<Integer> changes = new LinkedList<>();
+
+        /* save the input to a list */
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+
+            String currentLine = reader.readLine();
+            while (currentLine != null) {
+                int change = Integer.parseInt(currentLine);
+                changes.add(change);
+                currentLine = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        /* repeat until a duplicate frequency appears */
+        Set<Integer> frequencies = new HashSet<>();
+        int frequency = 0;
+        while (true) {
+            for (Integer change : changes) {
+                frequency += change;
+                if (frequencies.contains(frequency)) {
+                    return String.valueOf(frequency);
+                } else {
+                    frequencies.add(frequency);
+                }
+            }
+        }
+
+    }
+
+}
