05f16771ead0a8c0dcbc5f61b04fbdb7368e70ed
John Flanigan <johnrflanigan@gmail.com>
2018-12-12 01:39:33 -0500

    Refactored day 9, took inspiration from post on reddit

--- src/main/java/net/johnflanigan/adventofcode2018/day9/Day9.java
+++ src/main/java/net/johnflanigan/adventofcode2018/day9/Day9.java
@@ -3,77 +3,76 @@ package net.johnflanigan.adventofcode2018.day9;
 import net.johnflanigan.adventofcode2018.Day;
 
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day9 extends Day {
 
-    private static final int NUMBER_OF_PLAYERS = 476;
-    private static final long LAST_MARBLE = 71431 * 100;
-//    private static final int NUMBER_OF_PLAYERS = 9;
-//    private static final int LAST_MARBLE = 25;
-
-    LinkedList<Long> circle;
+    private class CircularQueue<T> extends LinkedList<T> {
+        /**
+         * @param turns positive turn to go clockwise, negative turn to go counterclockwise
+         */
+        private void rotate(int turns) {
+            if (turns > 0) {
+                for (int i = 0; i < turns; i++) {
+                    T val = this.removeLast();
+                    this.addFirst(val);
+                }
+            } else if (turns < 0) {
+                for (int i = 0; i < Math.abs(turns); i++) {
+                    T val = this.removeFirst();
+                    this.addLast(val);
+                }
+            }
+        }
+    }
 
     @Override
     public void solve() {
+        String file = "src/main/resources/day9_input.txt";
+        String input = readFile(file).get(0);
 
-//        String file = "src/main/resources/day9_input.txt";
-//        List<String> inputs = readFile(file);
+        String regex = "(\\d+) players; last marble is worth (\\d+) points";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
 
-        long[] playerScores = new long[NUMBER_OF_PLAYERS];
+        if (matcher.find()) {
+            int numberOfPlayers = Integer.parseInt(matcher.group(1));
+            int numberOfMarbles = Integer.parseInt(matcher.group(2));
 
-        circle = new LinkedList<>();
-        circle.add(0L);
+            System.out.println("Part 1 solution: " + game(numberOfPlayers, numberOfMarbles));
+            System.out.println("Part 2 solution: " + game(numberOfPlayers, numberOfMarbles * 100));
+        }
+    }
 
-        long currentMarble = 1;
+    private long game(int numberOfPlayers, int numberOfMarbles) {
+
+        long[] playerScores = new long[numberOfPlayers];
+
+        CircularQueue<Integer> circularQueue = new CircularQueue<>();
+        circularQueue.add(0);
+
         int currentPlayer = 0;
 
-        while (currentMarble <= LAST_MARBLE) {
+        for (int currentMarble = 1; currentMarble <= numberOfMarbles; currentMarble++) {
             // Is current marble a multiple of 23
             if (currentMarble % 23 == 0) {
                 // Move index seven counter-clockwise
-                rotate(-7);
-                long removedMarble = circle.removeLast();
+                circularQueue.rotate(-7);
+                long removedMarble = circularQueue.removeLast();
                 playerScores[currentPlayer] = playerScores[currentPlayer] + currentMarble + removedMarble;
             } else {
                 // Move current index two clockwise
-                rotate(2);
-                circle.addLast(currentMarble);
+                circularQueue.rotate(2);
+                circularQueue.addLast(currentMarble);
             }
-            currentMarble++;
             // Update current player
             currentPlayer++;
-            if (currentPlayer == NUMBER_OF_PLAYERS) {
+            if (currentPlayer == numberOfPlayers) {
                 currentPlayer = 0;
             }
         }
 
-        long highScore = 0;
-        for (long score : playerScores) {
-            System.out.println(score);
-            if (score > highScore) {
-                highScore = score;
-            }
-        }
-
-        System.out.println("Part 1 solution: " + highScore);
-    }
-
-    /**
-     *
-     * @param turns positive turn to go clockwise, negative turn to go counterclockwise
-     */
-    private void rotate(int turns) {
-        if (turns > 0) {
-            for (int i = 0; i < turns; i++) {
-                long val = circle.removeLast();
-                circle.addFirst(val);
-            }
-        } else if (turns < 0) {
-            for (int i = 0; i < Math.abs(turns); i++) {
-                long val = circle.removeFirst();
-                circle.addLast(val);
-            }
-        }
-
+        return Arrays.stream(playerScores).max().getAsLong();
     }
 }
